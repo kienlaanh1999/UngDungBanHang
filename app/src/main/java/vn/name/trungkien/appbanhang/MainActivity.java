76c36617997fe18ec381d7344f3ce91775958576
Nguyễn Đức Trung Kiên <k175520216025@tnut.edu.vn>
2023-02-22 22:34:13 +0700

    Initial commit

---
+++ app/src/main/java/vn/name/trungkien/appbanhang/MainActivity.java
@@ -0,0 +1,36 @@
+package vn.name.trungkien.appbanhang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewManHinhChinh;
+    NavigationView navigationView;
+    ListView listViewManHinhChinh;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AnhXa();
+    }
+
+    private void AnhXa() {
+        toolbar = findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper = findViewById(R.id.viewlipper);
+        recyclerViewManHinhChinh = findViewById(R.id.recycleview);
+        listViewManHinhChinh = findViewById(R.id.listviewmanhinhchinh);
+        navigationView = findViewById(R.id.navigationview);
+
+    }
+}
